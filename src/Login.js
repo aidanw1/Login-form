60a103f288fbe1e695d6868ddea5d61f0da8c3ec
Aidan Walker <aidan@aidanwalkersmbp.home>
2020-03-26 20:01:32 +0000

    first commit

---
+++ src/Login.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import './css/Login.scss'
+
+const Login = () => {
+
+  const [email, setEmail] = useState('')
+
+return (
+    <div className='loginContainer'>
+        <img src='./logo-r.svg' height={20} alt='green energy logo'/>
+        <h1 className='heading'>Example login screen </h1>
+        <h2 className='subHeader'>Getting started with Green.</h2>
+        <form onSubmit={e => {
+            e.preventDefault()
+            alert(`You have logged in using email: ${email}`)
+        }}>
+            <label className='email-label' htmlFor='email'>Email Address</label>
+            <input 
+            type='email' 
+            id='email' 
+            required
+            name='email'
+            onChange={e => setEmail(e.target.value)}/>
+            <div className='remember-device'>
+                <input type='checkbox'/>
+                <label className='remember-label'>Remember this device</label>
+            </div>
+            <button type='submit'>Sign In</button>
+        </form>
+        
+    </div>
+)
+}
+
+ 
+export default Login;
